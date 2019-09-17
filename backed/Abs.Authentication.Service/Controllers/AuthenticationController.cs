b3201d622ebb06105bdf2454eb6f06b8c6f54173
Miguel Roman <miguelerm@gmail.com>
2019-09-16 20:16:07 -0600

    Add cookie-sso authentication

---
+++ backed/Abs.Authentication.Service/Controllers/AuthenticationController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Minfin.SSO.Api.Models.TicketAutenticacion;
+using Minfin.SSO.WebApi.Client.Clients;
+
+namespace Abs.Authentication.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private const string AuthScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        private const string ClaimsNamespace = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/";
+
+        [HttpGet("")]
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            var identity = User.Identity;
+            var claims = User.Claims
+                .ToDictionary(
+                    claim => claim.Type.Replace(ClaimsNamespace, ""), 
+                    claim => claim.Value
+                );
+            return Ok(new { identity.IsAuthenticated, identity.Name, claims });
+        }
+
+        [HttpGet("validate")]
+        public async Task<IActionResult> ValidateAsync(
+            [FromQuery] string ticketAut, 
+            [FromServices] TicketAutenticacionClient ticketClient,
+            [FromServices] UsuarioClient usuarioClient,
+            [FromServices] AccesoClient accesoClient)
+        {
+            var result = ticketClient.Usar(ticketAut);
+
+            if (!result.Estado.Equals(EstadoRespuestaUsar.Ok))
+            {
+                return BadRequest(result);
+            }
+
+            var acceso = accesoClient.Obtener(result.Nit);
+
+            if (!acceso)
+            {
+                return BadRequest(new { result.Nit, acceso });
+            }
+
+            var usuario = usuarioClient.Obtener(result.Nit);
+
+            if (usuario == null || !usuario.Activo)
+            {
+                return BadRequest(new { result.Nit, usuario });
+            }
+
+            var claims = new List<Claim>
+            {
+              new Claim(ClaimTypes.Name, usuario.Nit),
+              new Claim(ClaimTypes.GivenName, usuario.Nombre),
+              new Claim(ClaimTypes.Email, usuario.Correo)
+            };
+
+            var identity = new ClaimsIdentity(claims, AuthScheme);
+            var properties = new AuthenticationProperties();
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(AuthScheme, principal, properties);
+
+            return Redirect("/");
+        }
+
+
+
+        [HttpGet("signout")]
+        public async Task<IActionResult> SignoutAsync()
+        {
+            await HttpContext.SignOutAsync(AuthScheme);
+            return Redirect("/");
+        }
+    }
+}
---
+++ backed/Abs.Authentication.Service/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Minfin.SSO.WebApi.Client;
+using Minfin.SSO.WebApi.Client.Clients;
+
+namespace Abs.Authentication.Service
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.Name = "sso_auth";
+                });
+
+            // SSO Dependencies:
+            var ssoConfig = Configuration.GetSection("SSO");
+            var privateKey = System.IO.File.ReadAllText(ssoConfig["PrivateKeyPath"]);
+            var serviceEndpointUri = new Uri(ssoConfig["ServiceEndpoint"]);
+
+            services.AddScoped(p => 
+                new AuthenticationManager(privateKey)
+            );
+
+            services.AddScoped(p => 
+                new TicketAutenticacionClient(serviceEndpointUri, p.GetRequiredService<AuthenticationManager>())
+            );
+
+            services.AddScoped(p => 
+                new UsuarioClient(serviceEndpointUri, p.GetRequiredService<AuthenticationManager>())
+            );
+            services.AddScoped(p =>
+                new AccesoClient(serviceEndpointUri, p.GetRequiredService<AuthenticationManager>())
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
